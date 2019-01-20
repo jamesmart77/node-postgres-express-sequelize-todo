4d1747af4262094cbce0111b8c408a57e748b155
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-01-20 14:01:27 -0500

    initial commit of todo project -- server working

---
+++ bin/server.js
@@ -0,0 +1,11 @@
+// This will be our application entry. We'll setup our server here.
+const http = require('http');
+
+// The express app we just created
+const app = require('../express');
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
