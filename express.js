f2bf360eb4c87934b96f0ecd5f99f396b997572c
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-01-20 19:17:57 -0500

    all routes added and operational

--- express.js
+++ express.js
@@ -12,6 +12,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+// Require routes into app
+require('./routes')(app);
+
 // Setup a default catch-all route that sends back a welcome message in JSON
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to the beginning of the todo app!!',
